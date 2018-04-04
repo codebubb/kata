4467ec21294efc04065599080d75e3df6e769a18
James Bubb <jpbubb82@gmial.com>
2018-04-04 17:56:16 +0100

    Getting somewhere

--- human-readable-duration-format/solution.js
+++ human-readable-duration-format/solution.js
@@ -8,32 +8,27 @@ function humanReadableDuration(n) {
     if (n === 0) return 'now';
     const MINUTE = 60;
     const HOUR = 60 * MINUTE;
-    const DAYS = 24 * HOUR;
+    const DAY = 24 * HOUR;
+    // const WEEK = 7 * DAY;
+    const YEAR = 365 * DAY;
+    const times = [{key: 'year', val: YEAR}, {key: 'day', val: DAY}, {key: 'hour', val: HOUR}, {key: 'minute', val:  MINUTE}, {key: 'second', val: 1}]
+        .map(
+            time => {
+                const val = Math.floor(n / time.val);
+                n -= val * time.val;
+                return {key: time.key, val: val};
+            }
+        )
+        .filter(
+            time => time.val > 0
+        )
+        .reduce(
+            (acc, time, index, arr) => {
+                return `${acc}${time.val} ${time.val > 1 ? time.key + 's' : time.key}${index === arr.length -1 ? '' : index < arr.length -2   ? ', ': ' and '}`;
+            }, ''
+        )
 
-    const times = (num) => {
-        this.baseTime = num;
-        this.days = () => Math.floor(this.baseTime / DAYS),
-        this.hours = () => Math.floor((this.baseTime - this.days * DAYS) / HOUR);
-        this.minutes = () => Math.floor((this.baseTime - ((this.days * DAYS) + this.hours * HOURS)));
-        this.seconds = () => this.baseTime % MINUTE;
-    };
 
-
-    let returnStr = '';
-    const returnArr = [];
-
-  
-
-    console.log('Times: ', times.days.call({baseTime: n}));
-    
-    
-    for(let i=0; i<returnArr.length; i++) {
-        returnStr += `${returnArr[i].value} ${returnArr[i].value > 1 ? returnArr[i].key + 's' : returnArr[i].key}`;
-        if(i < returnArr.length -1) returnStr += ', ';
-        if(i === returnArr.length -1) returnStr += ' and ';
-    }
-
-    console.log('Returnstr: ', returnStr);
-    return returnStr;
+    return times;
     
 }
