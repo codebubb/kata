51e75cf36fbfe3fa332ddfed2ba22d67cc20c980
James Bubb <jpbubb82@gmail.com>
2018-04-06 14:07:43 +0100

    Need to rethink how to traverse both strings simultaneously

--- merged-string-checker/solution.js
+++ merged-string-checker/solution.js
@@ -1,15 +1,15 @@
 module.exports = { isMerge };
 
 function isMerge(string, part1, part2) {
-    const parts = `${part1}${part2}`;
+    const parts = `${part1}${part2}`.split('');
     const matching = string.split('').reduce(
         (acc, letter) => {
             const letterInParts = parts.indexOf(letter);
-            if (letterInParts > -1) {
-                parts.slice(letterInParts, 1);
+            if (letterInParts > -1 &&) {
+                parts.splice(letterInParts, 1);
                 return acc + letter;
             }
-        }
+        }, ''
     );
-    return matching === string;
+    return parts.length === 0 && matching === string;
 }
--- merged-string-checker/spec.js
+++ merged-string-checker/spec.js
@@ -14,14 +14,26 @@ describe('can merge strings', () => {
     });
 
     it('should merge happ y', () => {
-        expect(solution.isMerge('happy', 'happ', 'y'));
+        expect(solution.isMerge('happy', 'happ', 'y')).toBe(true);
     })
 
     it('should merge ypaph thbiryad', () => {
-        expect(solution.isMerge('happybirthday', 'ypaph', 'thbiryad'));
+        expect(solution.isMerge('happybirthday', 'ypaph', 'thbiryad')).toBe(true);
     })
 
     it('should not merge abc def', () => {
-        expect(solution.isMerge('abcdefg', 'abc', 'def'));
+        expect(solution.isMerge('abcdefg', 'abc', 'def')).toBe(false);
     })
+
+    it('should merge empty strings', () => {
+        expect(solution.isMerge('', '', '')).toBe(true);
+    });
+
+    it('should merge when one string is empty', () => {
+        expect(solution.isMerge('abc', 'abc', '')).toBe(true);
+    });
+
+    it('should merge when one string is longer than the other', () => {
+        expect(solution.isMerge('abcdef', 'abc', 'defg')).toBe(false);
+    });
 });
